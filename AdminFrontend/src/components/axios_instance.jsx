de80954ae1f36919f333ca31e0f72ba9b2c3ccea
RAHUL GUPTA <rahul01.org@gmail.com>
2026-02-23 12:53:44 +0530

    feat: create AdminFrontend axios_instance.jsx with VITE_API_URL and admin token interceptor

---
+++ AdminFrontend/src/components/axios_instance.jsx
@@ -0,0 +1,20 @@
+// axios_instance.jsx
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
+});
+
+// Attach admin token automatically if present
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("adminToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default instance;
